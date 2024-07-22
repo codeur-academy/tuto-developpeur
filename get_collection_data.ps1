7fbd5d8374dc36cee4f85fea002c88cdc5bcb054
ESSARRAJ <essarraj.fouad@gmail.com>
2024-07-22 10:17:45 +0100

    save

---
+++ get_collection_data.ps1
@@ -0,0 +1,4 @@
+
+# fihcier google sheet publier =  https://docs.google.com/spreadsheets/d/e/2PACX-1vSOU2favoVi2dojkph7iMx-YBVbbtpwUWf6nnw-dxYdhKWcEZjxa5ZZH-ARKChhbejooKZ2wGWdbDE3/pub?output=xlsx
+
+Invoke-WebRequest -Uri https://docs.google.com/spreadsheets/d/e/2PACX-1vSOU2favoVi2dojkph7iMx-YBVbbtpwUWf6nnw-dxYdhKWcEZjxa5ZZH-ARKChhbejooKZ2wGWdbDE3/pub?output=xlsx -OutFile collection.xlsx
